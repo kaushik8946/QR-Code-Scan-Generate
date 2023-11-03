a7a83397df681c45b2b92f99a37e9d4c369833ce
kaushik8946 <suryakaushik2003@gmail.com>
2023-11-03 22:25:04 +0530

    first commit

---
+++ app/src/main/java/com/kaushik/qr_code/Generator.kt
@@ -0,0 +1,20 @@
+package com.kaushik.qr_code
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+class Generator : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
